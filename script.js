1bd7773e9a21643d5abd41ca2abd7bf0dcdd5eba
Edem0088 <109944353+Edem0088@users.noreply.github.com>
2022-11-30 20:03:55 +0500

    vbdbhbb

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ red.style.opacity = '1';
 yellow.style.opacity = '0.8';
 green.style.opacity = '0.8';
 
-red.addEventListener('onclick', ()=>{
-    red.style.opacity = '0.8';
-    yellow.style.opacity = '1';
-})
-yellow.addEventListener('onclick', ()=>{
-    yellow.style.opacity = '0.8';
-    green.style.opacity = '1';
-})
-green.addEventListener('onclick', ()=>{
-    green.style.opacity = '0.8';
-    red.style.opacity = '1';
-})
+
+function Color(color){
+    if(color == 'red'){
+        red.style.opacity = '0.8';
+        yellow.style.opacity = '1';
+    }
+    else if (color == 'yellow'){
+        yellow.style.opacity = '0.8';
+        green.style.opacity = '1';
+    }
+    else{
+        green.style.opacity = '0.8';
+        red.style.opacity = '1';
+    }
+}
