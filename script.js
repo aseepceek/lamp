28c99c68a56469911395d4502bee2f1ad015064f
Edem0088 <109944353+Edem0088@users.noreply.github.com>
2022-11-30 20:00:57 +0500

    dsklgvjjkhfv

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ let red = document.querySelector('.red');
 let yellow = document.querySelector('.yellow');
 let green = document.querySelector('.green');
 
-red.style.opocity = '1';
-yellow.style.opocity = '0.8';
-green.style.opocity = '0.8';
+red.style.opacity = '1';
+yellow.style.opacity = '0.8';
+green.style.opacity = '0.8';
 
 red.addEventListener('onclick', ()=>{
     red.style.opacity = '0.8';
     yellow.style.opacity = '1';
 })
-yellow.addEventListener(onclick, ()=>{
+yellow.addEventListener('onclick', ()=>{
     yellow.style.opacity = '0.8';
     green.style.opacity = '1';
 })
